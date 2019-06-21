a84c4ce53951deaa59072b67a6857059de62901b
Jay Spain <annakirklandsmith@gmail.com>
2019-06-20 22:14:52 -0400

    five pages drafted

---
+++ script.js
@@ -0,0 +1,148 @@
+document.addEventListener('DOMContentLoaded', function () {
+   
+
+
+
+    const header = document.getElementById('headerDiv');
+    header.innerHTML = 
+
+    `<div class="mdc-tab-bar" role="tablist">
+        <div class="mdc-tab-scroller">
+            <div class="mdc-tab-scroller__scroll-area">
+                <div class="mdc-tab-scroller__scroll-content">
+
+                <button id="programsbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='index.html'">
+                <span class="mdc-tab__content">
+                    <span class="mdc-tab__text-label">Home</span>
+                </span>
+                <span id="programsindicator" class="mdc-tab-indicator mdc-tab-indicator--active">
+                    <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                </span>
+                <span class="mdc-tab__ripple"></span>
+                </button>
+
+                <button id="programsbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='scientista.html'">
+                <span class="mdc-tab__content">
+                    <span class="mdc-tab__text-label">Scientista?</span>
+                </span>
+                <span id="programsindicator" class="mdc-tab-indicator mdc-tab-indicator--active">
+                    <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                </span>
+                <span class="mdc-tab__ripple"></span>
+                </button>
+
+                <button id="programsbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='programs.html'">
+                    <span class="mdc-tab__content">
+                        <span class="mdc-tab__text-label">Programs</span>
+                    </span>
+                    <span id="programsindicator" class="mdc-tab-indicator mdc-tab-indicator--active">
+                        <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                    </span>
+                    <span class="mdc-tab__ripple"></span>
+                </button>
+
+            <button id="valuesbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='values.html'">
+                <span class="mdc-tab__content">
+                    <span class="mdc-tab__text-label">Values</span>
+                </span>
+                <span id="valuesindicator" class="mdc-tab-indicator mdc-tab-indicator--active">
+                    <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                </span>
+                <span class="mdc-tab__ripple"></span>
+            </button>
+
+
+            <button id="valuesbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='allies.html'">
+                <span class="mdc-tab__content">
+                    <span class="mdc-tab__text-label">Allies</span>
+                </span>
+                <span id="valuesindicator" class="mdc-tab-indicator mdc-tab-indicator--active">
+                    <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                </span>
+                <span class="mdc-tab__ripple"></span>
+            </button>
+
+            <button id="valuesbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='redcoat.html'">
+                <span class="mdc-tab__content">
+                    <span class="mdc-tab__text-label">Redcoat</span>
+                </span>
+                <span id="valuesindicator" class="mdc-tab-indicator mdc-tab-indicator--active">
+                    <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                </span>
+                <span class="mdc-tab__ripple"></span>
+            </button>
+
+            <button id="valuesbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='futurefair.html'">
+                <span class="mdc-tab__content">
+                    <span class="mdc-tab__text-label">Future Fair</span>
+                </span>
+                <span id="valuesindicator" class="mdc-tab-indicator mdc-tab-indicator--active">
+                    <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+                </span>
+                <span class="mdc-tab__ripple"></span>
+            </button>
+                
+                
+                </div>
+            </div>
+        </div>
+    </div>`
+    
+    const footer = document.getElementById('footerDiv');
+    footer.innerHTML = 
+
+
+    `<div class="footer">
+    <p tabindex="0" class="mdc-typography--body1" >Data Scientista Society</p>
+
+    <div>
+    <a class="mdc-typography--body1" href="https://www.meetup.com/DataScientistaSociety/" target='blank'>
+        <img src="images/social-media-icons/meetup.png" alt="Meetup" />
+    </a>
+
+    <a  class="mdc-typography--body1" href="https://www.linkedin.com/company/data-scientista-society" target='blank'>
+        <img src="images/social-media-icons/linkedin.png" alt="Linkedin" />
+    </a>
+
+    <a class="mdc-typography--body1" href="mailto:datascientistasociety@gmail.com?subject=Message from website" target='blank'>
+        <img src="images/social-media-icons/email.png" alt="Mail" />
+    </a>
+
+</a>
+</div>
+
+
+    <p tabindex="0" class="mdc-typography--body1 white">
+
+    Email: <a class="mdc-typography--body1 white" href="mailto:datascientistasociety@gmail.com">datascientistasociety@gmail.com</a></p>
+
+        
+
+
+                    
+
+
+<!-- https://mycyberuniverse.com/add-social-media-buttons-to-your-website.html -->
+
+
+    
+
+       
+
+    
+    
+    <div>
+    <a class="mdc-typography--body1 white" href="index.html" class="">Home</a>
+    <a class="mdc-typography--body1 white" href="programs.html" class="">Programs</a>
+    <a class="mdc-typography--body1 white" href="values.html" class="">Values</a>
+    <a class="mdc-typography--body1 white" href="ourstory.html" class="">Our Story</a>
+    <a class="mdc-typography--body1 white" href="aboutus.html" class="">About Us</a>
+    <a class="mdc-typography--body1 white" href="allies.html" class="">Allies</a>
+    <a class="mdc-typography--body1 white" href="redcoat.html" class="">Redcoat</a>
+    <a class="mdc-typography--body1 white" href="futurefair.html" class="">Future Fair</a>
+    </div>
+    `
+
+
+
+})
