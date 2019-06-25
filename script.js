9654d03bbece23907cd3b6fcdd20da3dc9033906
Jay Spain <annakirklandsmith@gmail.com>
2019-06-25 19:22:27 -0400

    random work

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const header = document.getElementById('headerDiv');
     header.innerHTML = 
 
-    `<div class="mdc-tab-bar" role="tablist">
+    `<div class="mdc-tab-bar mdc-top-app-bar mdc-top-app-bar--fixed" role="tablist">
         <div class="mdc-tab-scroller">
             <div class="mdc-tab-scroller__scroll-area">
                 <div class="mdc-tab-scroller__scroll-content">
 
                 <button id="programsbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='index.html'">
                 <span class="mdc-tab__content">
-                    <span class="mdc-tab__text-label">Home</span>
+                    <span class="mdc-tab__text-label"><img tabindex="0" style="width:2rem" src="logosquare2.png" aria-label="coat button"></span>
                 </span>
                 <span id="programsindicator" class="mdc-tab-indicator mdc-tab-indicator--active">
                     <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
@@ -41,9 +41,9 @@ document.addEventListener('DOMContentLoaded', function () {
                     <span class="mdc-tab__ripple"></span>
                 </button>
 
-            <button id="valuesbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='values.html'">
+            <button id="valuesbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='aboutus.html'">
                 <span class="mdc-tab__content">
-                    <span class="mdc-tab__text-label">Values</span>
+                    <span class="mdc-tab__text-label">About Us</span>
                 </span>
                 <span id="valuesindicator" class="mdc-tab-indicator mdc-tab-indicator--active">
                     <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
@@ -62,6 +62,16 @@ document.addEventListener('DOMContentLoaded', function () {
                 <span class="mdc-tab__ripple"></span>
             </button>
 
+        <button id="valuesbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='futurefair.html'">
+            <span class="mdc-tab__content">
+                <span class="mdc-tab__text-label">Future Fair</span>
+            </span>
+            <span id="valuesindicator" class="mdc-tab-indicator mdc-tab-indicator--active">
+                <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+            </span>
+            <span class="mdc-tab__ripple"></span>
+        </button>
+
             <button id="valuesbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='redcoat.html'">
                 <span class="mdc-tab__content">
                     <span class="mdc-tab__text-label">Redcoat</span>
@@ -72,21 +82,14 @@ document.addEventListener('DOMContentLoaded', function () {
                 <span class="mdc-tab__ripple"></span>
             </button>
 
-            <button id="valuesbutton" class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0" onClick="location.href='futurefair.html'">
-                <span class="mdc-tab__content">
-                    <span class="mdc-tab__text-label">Future Fair</span>
-                </span>
-                <span id="valuesindicator" class="mdc-tab-indicator mdc-tab-indicator--active">
-                    <span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
-                </span>
-                <span class="mdc-tab__ripple"></span>
-            </button>
+
                 
                 
                 </div>
             </div>
         </div>
-    </div>`
+    </div>
+    <div class="mdc-top-app-bar--fixed-adjust"></div>`
     
     const footer = document.getElementById('footerDiv');
     footer.innerHTML = 
